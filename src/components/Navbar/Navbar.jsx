19a9f8fb3057e7a7d94dfff51b64fd024f93fa8a
manasjhamj <jhamanas939@gmail.com>
2024-05-31 13:30:34 +0530

    add autoscroll to carousel

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
 
 function Navbar() {
+
+    const [colorChange, setColorchange] = useState(false);
+
+    const changeNavbarColor = () => {
+        if (window.scrollY >= 200) {
+            setColorchange(true);
+        } else {
+            setColorchange(false);
+        }
+    };
+
+    window.addEventListener("scroll", changeNavbarColor);
+
     return (
-        <nav>
+        <nav
+            className={
+                colorChange
+                    ? "navbar colorChange"
+                    : "navbar"
+            }
+        >
             <div className='logo'>
                 <h1>FRAMEWORK</h1>
             </div>
@@ -12,8 +31,9 @@ function Navbar() {
                 <a href="">About</a>
                 <a href="">Contact</a>
             </div>
-            <div className='work-btn'>
-                <a href="">Hire Us</a>
+            <div className='work-cta'>
+                <button className="work-btn" role="button">Book us</button>
+
             </div>
         </nav>
     )
